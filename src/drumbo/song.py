83e76524fb6eae4a4d98958b0ea48a3e0da86379
accraze <accraze@gmail.com>
2018-03-09 19:30:55 -0800

    refactor step output and add docstrings

--- src/drumbo/song.py
+++ src/drumbo/song.py
@@ -12,24 +12,33 @@ class Song:
     def add(self, drum):
         """
         Adds a drum pattern to the song.
-        If the pattern's length does not match
-        the song's step count, we throw
-        an error.
+        Raise error if pattern length
+        does not match global steps count.
         """
         if not self._matching_steps(drum.steps):
             raise Exception('Incorrect pattern step amount')
         self.drums.append(drum)
 
     def play(self):
-        self._print('|')
+        """
+        Plays all the song's
+        patterns in real-time:
+        |Kick|_|HiHat|_|Kick+Snare|_|...
+        """
+        self._print('|', delay=False)
         for step in range(self.steps):
-            note = '{}'.format(self._step_output(step))
+            note = self._get_step_output(step)
             self._print(note + '|')
+            # line break for every bar
             if (step + 1) % self.steps == 0:
                 self._print('\n', delay=False)
 
-    def _print(self, letter, delay=True):
-        sys.stdout.write(letter)
+    def _print(self, msg, delay=True):
+        """
+        Prints a step's note(s) to
+        stdout for tempo-timed output.
+        """
+        sys.stdout.write(msg)
         sys.stdout.flush()
         if delay:
             import time
@@ -37,23 +46,48 @@ class Song:
 
     @property
     def _timing_delay(self):
+        """
+        Calculates how many
+        seconds for each step.
+        """
         return (60/self.bpm) * .5
 
     def _matching_steps(self, pattern):
+        """
+        Checks if a given pattern
+        matches the song's global
+        step count.
+        """
         if len(pattern) != self.steps:
             return False
         return True
 
-    def _step_output(self, step):
+    def _get_step_output(self, step):
+        """
+        Returns notes played
+        for each step as string
+        otherwise, return '_'
+        """
+        output = self._render_output(step)
+        if not output:
+            output = '_'
+        return output
+
+    def _render_output(self, step):
+        """
+        Combines all instrument names
+        for a given step.
+        """
         output = ''
         multi = False
         for drum in self.drums:
             if drum.steps[step] != '_':
                 if multi:
+                    # more than one instrument played
+                    # on current step, so concatenate
                     output += '+{}'.format(drum.name)
                 else:
                     output += drum.name
                     multi = True
-        if not output:
-            output = '_'
         return output
+
